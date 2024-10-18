f5c70242f742da9872c8ef0ce574fdbc13ad2f86
Vincent Mitchellgit config --global --listgit config --global user.name Vincent <Vincent.mitchell@stu.ocsb.ca>
2024-10-18 10:14:37 -0400

    10/18/2024

--- main.js
+++ main.js
@@ -9,8 +9,11 @@
 'use strict';
 
 /*** Event Listeners ***/
-
-
+document.getElementById("d6").addEventListener("click", roll_d6)
+document.getElementById("d8").addEventListener("click", roll_d8)
+document.getElementById("d10").addEventListener("click",roll_d10)
+document.getElementById("d12").addEventListener("click",roll_d12)
+document.getElementById("d20").addEventListener("click",roll_d20)
 /*** Functions ***/
 
 // Return a random integer from `min` to `max`, inclusive
@@ -18,3 +21,28 @@ function randInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+
+function roll_d6() { 
+    let roll = randInt (1, 6)
+    document.getElementById("die_roll").textContent = roll
+}
+
+function roll_d8() { 
+    let roll = randInt (1, 8)
+    document.getElementById("die_roll").textContent = roll
+}
+
+function roll_d10() { 
+    let roll = randInt (1, 10)
+    document.getElementById("die_roll").textContent = roll
+}
+
+function roll_d12() { 
+    let roll = randInt (1, 12)
+    document.getElementById("die_roll").textContent = roll
+}
+
+function roll_d20() { 
+    let roll = randInt (1, 20)
+    document.getElementById("die_roll").textContent = roll
+}
